c30744301fc3309596ebcd35ecdb74a94fb16f24
Niranjan155 <niruhegde155@gmail.com>
2021-01-03 16:31:01 +0530

    web_chat

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
-#class Messages:
+class Messages(models.Model):
+    msg_from=models.ForeignKey(User,on_delete=models.CASCADE)
+    msg_to=models.ForeignKey(User,on_delete=models.CASCADE)
+    msg_text=models.TextField()
+    date_postdate=models.DateTimeField(default=datetime.now)
+    def __str__(self):
+        return self.msg_text
+
 
